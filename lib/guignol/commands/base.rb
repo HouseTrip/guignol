76fd03b0a2e7c1e3efa49586509bbe5e6626707f
Stephen Creedon <sc@housetrip.com>
2012-08-28 18:42:21 +0100

    added arguments list at the end of server list and parsed arguments into servers and args. Args are every ARGV options after the first --named-arg

--- lib/guignol/commands/base.rb
+++ lib/guignol/commands/base.rb
@@ -32,15 +32,24 @@ require 'guignol/array/collect_key'
 
 module Guignol::Commands
   class Base
+    class Error < Exception; end
+    
+    def self.ensure_args(*args)
+      self.class_variable_set(:@@ensure_args, args.flatten)
+    end
+    
+    NAMED_ARG = /^--/
 
     def initialize(*argv)
+      @argv = argv.flatten
       @all_configs = load_config_files
       check_config_consistency
-      @configs = argv.map { |pattern| 
+      @configs = servers.map { |pattern| 
         @all_configs.select { |config|
           config[:name] =~ /#{pattern}/
         }
       }.flatten.uniq
+      ensure_args
     end
 
     def run
@@ -51,8 +60,26 @@ module Guignol::Commands
       end
     end
 
-  protected
+    def servers
+      @servers ||= _servers
+    end
+    
+    def args
+      @args ||= _args
+    end
+    
+    def arg_val(arg_name)
+      return nil unless args.include?(arg_name)
+      next_arg(arg_name)
+    end
+    
+    def arg?(arg_name)
+      args.include?(arg_name)
+    end
+    
 
+  protected
+  
     def configs
       @all_configs
     end
@@ -64,8 +91,33 @@ module Guignol::Commands
       answer = $stdin.gets
       return answer.strip =~ /y/i
     end
+    
+    def ensure_args
+      return unless self.class.class_variable_defined?(:@@ensure_args)
+ 
+      self.class.class_variable_get(:@@ensure_args).each do |req_arg|
+        raise Error.new("required argument #{req_arg} not found") unless args.include?(req_arg)
+      end
+    end
 
   private
+  
+    def _servers
+      @argv - args
+    end
+    
+    def _args
+      @argv.dup.drop_while do |arg|
+        (arg =~ NAMED_ARG).nil?
+      end
+    end
+    
+    def next_arg(arg_name)
+      index = args.index(arg_name)
+      arg_at = args[index + 1]
+      return nil if arg_at =~ NAMED_ARG
+      arg_at
+    end
 
     # Read & return the first available config.
     def load_config_files
