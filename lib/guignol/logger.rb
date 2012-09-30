dbe53ac569d50c5585bb5e2d9b4a788d53e89a6c
Julien Letessier <julien.letessier@gmail.com>
2012-09-30 17:06:49 +0100

    Cleaner logging

--- lib/guignol/logger.rb
+++ lib/guignol/logger.rb
@@ -4,11 +4,20 @@ module Guignol::Logger
   def logger
     @logger ||= ::Logger.new(logger_file).tap do |logger|
       logger.progname = 'guignol'
+      logger.formatter = Formatter.new
     end
   end
 
 
   private
+
+  class Formatter < ::Logger::Formatter
+    Format = "[%s] %s: %s\n"
+
+    def call(severity, time, progname, msg)
+      Format % [time.strftime('%F %T'), severity, msg2str(msg)]
+    end
+  end
 
 
   def logger_file
