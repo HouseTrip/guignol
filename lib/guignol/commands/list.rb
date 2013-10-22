868ba939b19ec5c2efefedd855b555fe6dcf79a4
Mark Connell <mark@markconnell.co.uk>
2013-10-22 16:28:46 +0100

    add command line options --elba and --with_instance_ids

--- lib/guignol/commands/list.rb
+++ lib/guignol/commands/list.rb
@@ -2,9 +2,12 @@ require 'guignol/commands/base'
 
 Guignol::Shell.class_eval do
   desc 'list [PATTERNS]', 'List the status of all known instances'
+
+  option :elba, :type => :boolean, :aliases => :e
+  option :with_instance_ids, :type => :boolean, :aliases => :i
   def list(*patterns)
     patterns.push('.*') if patterns.empty?
-    Guignol::Commands::List.new(patterns).run
+    Guignol::Commands::List.new(patterns, options).run
   end
 end
 
@@ -13,14 +16,28 @@ module Guignol::Commands
     private
 
     def run_on_server(instance, options = {})
+      return output_elba_friendly_instance_ids(instance) if options[:elba]
+
       synchronize do
-        id = instance.id || 'unknown'
-        shell.say id.ljust(@max_width)
+        if options[:with_instance_ids]
+          id = "#{ instance.id || 'unknown'} "
+          shell.say id.ljust(@max_width)
+        end
 
         shell.say instance.name.ljust(@max_width + 1)
         shell.say instance.state, colorize(instance.state)
       end
     end
+
+    def output_elba_friendly_instance_ids(instance)
+      return if instance.id.blank?
+      synchronize { shell.say "#{instance.id} " }
+    end
+
+    def after_run(data, options)
+      shell.say "\n"
+    end
+
 
     def before_run(configs, options = {})
       @max_width = configs.keys.map(&:size).max
