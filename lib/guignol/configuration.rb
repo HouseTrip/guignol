287c31135a8c27b950f3f3e861d597acbf23b4a9
Mark Connell <mark@markconnell.co.uk>
2013-10-23 10:49:48 +0100

    use nested modules to prevent undefined module errors in ruby 2.0

--- lib/guignol/configuration.rb
+++ lib/guignol/configuration.rb
@@ -3,41 +3,43 @@ require 'active_support'
 require 'active_support/core_ext/enumerable'
 require 'guignol'
 
-module Guignol::Configuration
+module Guignol
+  module Configuration
 
-  def configuration
-    @configuration ||= load_config_file
-  end
-
-  private
-
-  def config_file_path
-    @config_file_path ||= [
-      Pathname.new(ENV['GUIGNOL_YML'] || '/var/nonexistent'),
-      Pathname.new('guignol.yml'),
-      Pathname.new('config/guignol.yml'),
-      Pathname.new(ENV['HOME']).join('.guignol.yml')
-    ].find(&:exist?)
-  end
-
-  # Load the config hash for the file, converting old (v0.2.0) Yaml config files.
-  def load_config_file
-    return {} if config_file_path.nil?
-    data = YAML.load(config_file_path.read)
-    return data unless data.kind_of?(Array)
-
-    # Convert the toplevel array to a hash. Same for arrays of volumes.
-    Guignol.logger.warn "Configuration file '#{config_file_path}' uses the old array format. Trying to load it."
-    raise "Instance config lacks :name" unless data.collect_key(:name).all?
-    result = data.index_by { |item| item.delete(:name) }
-    result.each_pair do |name, config|
-      next unless config[:volumes]
-      raise "Volume config lacks :name" unless config[:volumes].collect_key(:name).all?
-      config[:volumes] = config[:volumes].index_by { |item| item.delete(:name) }
+    def configuration
+      @configuration ||= load_config_file
     end
 
-    return result
-  end
+    private
 
-  Guignol.extend(self)
+    def config_file_path
+      @config_file_path ||= [
+        Pathname.new(ENV['GUIGNOL_YML'] || '/var/nonexistent'),
+        Pathname.new('guignol.yml'),
+        Pathname.new('config/guignol.yml'),
+        Pathname.new(ENV['HOME']).join('.guignol.yml')
+      ].find(&:exist?)
+    end
+
+    # Load the config hash for the file, converting old (v0.2.0) Yaml config files.
+    def load_config_file
+      return {} if config_file_path.nil?
+      data = YAML.load(config_file_path.read)
+      return data unless data.kind_of?(Array)
+
+      # Convert the toplevel array to a hash. Same for arrays of volumes.
+      Guignol.logger.warn "Configuration file '#{config_file_path}' uses the old array format. Trying to load it."
+      raise "Instance config lacks :name" unless data.collect_key(:name).all?
+      result = data.index_by { |item| item.delete(:name) }
+      result.each_pair do |name, config|
+        next unless config[:volumes]
+        raise "Volume config lacks :name" unless config[:volumes].collect_key(:name).all?
+        config[:volumes] = config[:volumes].index_by { |item| item.delete(:name) }
+      end
+
+      return result
+    end
+
+    Guignol.extend(self)
+  end
 end
--- lib/guignol/logger.rb
+++ lib/guignol/logger.rb
@@ -1,29 +1,31 @@
 require 'logger'
 
-module Guignol::Logger
-  def logger
-    @logger ||= ::Logger.new(logger_file).tap do |logger|
-      logger.progname = 'guignol'
-      logger.formatter = Formatter.new
+module Guignol
+  module Logger
+    def logger
+      @logger ||= ::Logger.new(logger_file).tap do |logger|
+        logger.progname = 'guignol'
+        logger.formatter = Formatter.new
+      end
     end
-  end
 
 
-  private
+    private
 
-  class Formatter < ::Logger::Formatter
-    Format = "[%s] %s: %s\n"
+    class Formatter < ::Logger::Formatter
+      Format = "[%s] %s: %s\n"
 
-    def call(severity, time, progname, msg)
-      Format % [time.strftime('%F %T'), severity, msg2str(msg)]
+      def call(severity, time, progname, msg)
+        Format % [time.strftime('%F %T'), severity, msg2str(msg)]
+      end
     end
+
+
+    def logger_file
+      return File.open(ENV['GUIGNOL_LOG'] ,'a') if ENV['GUIGNOL_LOG']
+      $stdout.tty? ? $stdout : File.open('/dev/null','w')
+    end
+
+    Guignol.extend(self)
   end
-
-
-  def logger_file
-    return File.open(ENV['GUIGNOL_LOG'] ,'a') if ENV['GUIGNOL_LOG'] 
-    $stdout.tty? ? $stdout : File.open('/dev/null','w')  
-  end
-
-  Guignol.extend(self)
 end
